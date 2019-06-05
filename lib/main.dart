3eeb917c4ab9517f1c375108300707630965cf2e
roccosi <mibcreative@gmail.com>
2019-06-05 17:28:47 +0200

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[800],
+        ),
+        body: EightBall(),
+      ),
+    ),
+  );
+}
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int imageNumber = 1;
+
+  void changeTheNumber() {
+    imageNumber = Random().nextInt(5) + 1;
+    print('$imageNumber');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          child: Image.asset('images/ball$imageNumber.png'),
+          onPressed: () {
+            setState(() {
+              changeTheNumber();
+            });
+          },
+        ),
       ),
     );
+  }
+}
